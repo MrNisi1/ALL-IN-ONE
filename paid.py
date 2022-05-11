cae3de694a18f43e4117e39896307b62a0869990
MrNisi1 <104666224+MrNisi1@users.noreply.github.com>
2022-05-11 18:46:44 +0600

    Update paid.py

--- paid.py
+++ paid.py
@@ -11,7 +11,7 @@ if bit == '64bit':
     os.system('clear')
     print ('WELCOME 64 BIT PAID USER..!')
     sleep (2)
-    from MAL64 import hello
+    from Mal64 import hello
     ()
 
 elif bit == '32bit':
